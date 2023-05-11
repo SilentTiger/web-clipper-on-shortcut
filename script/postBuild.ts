cb8dad6a7655368ce18e10254aeea2ddfee6ac6e
津玮 <liujinwei@shimo.im>
2023-05-11 18:20:03 +0800

    change version order

--- script/postBuild.ts
+++ script/postBuild.ts
@@ -6,10 +6,10 @@ const readabilityVersion = dependencies['@mozilla/readability']
 const dompurifyVersion = dependencies['dompurify']
 const turndownVersion = dependencies['turndown']
 
-const versionFileContent = `https://silenttiger.online/web-clipper-on-shortcut/index-${mainVersion}.js
-https://cdn.jsdelivr.net/npm/@mozilla/readability@${readabilityVersion}/Readability.js
+const versionFileContent = `https://cdn.jsdelivr.net/npm/@mozilla/readability@${readabilityVersion}/Readability.js
 https://cdn.jsdelivr.net/npm/dompurify@${dompurifyVersion}/dist/purify.min.js
-https://cdn.jsdelivr.net/npm/turndown@${turndownVersion}/dist/turndown.js`
+https://cdn.jsdelivr.net/npm/turndown@${turndownVersion}/dist/turndown.js
+https://silenttiger.online/web-clipper-on-shortcut/index-${mainVersion}.js`
 console.log('versionFileContent', versionFileContent)
 writeFileSync('./website/version.txt', versionFileContent)
 renameSync('./dist/index.js', `./website/index-${mainVersion}.js`)
