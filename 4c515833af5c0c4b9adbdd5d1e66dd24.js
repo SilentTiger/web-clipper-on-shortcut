1556b1efc905e72ace9d19ce4880f531b711b604
SilentTiger <cia.fbi.007@hotmail.com>
2023-05-12 09:46:19 +0000

    Deploying to gh-pages from @ SilentTiger/web-clipper-on-shortcut@301982654abdf9d0fe6e6dcac28f72b023957c2b 🚀

--- 4c515833af5c0c4b9adbdd5d1e66dd24.js
+++ 4c515833af5c0c4b9adbdd5d1e66dd24.js
@@ -2833,8 +2833,10 @@
       const vault = config.split("|")[0];
       const name = config.split("|")[1];
       if (vault && name) {
-        window.open(`obsidian://new?vault=${vault}&file=${name}/${article.title}`);
-        return article.markdownContent;
+        return [
+          `obsidian://new?vault=${vault}&file=${name}/${article.title}`,
+          article.markdownContent
+        ];
       }
       return null;
     }
