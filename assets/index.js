88f265e01add9d33408a25db140f1ef3d7273584
SilentTiger <cia.fbi.007@hotmail.com>
2023-05-18 07:00:21 +0000

    Deploying to gh-pages from @ SilentTiger/web-clipper-on-shortcut@65fd84b4da0bbcc606a49818c6cbbcec9e3ef312 🚀

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,57 @@
-function addScrollingClass() {
-  document.body.classList.add("scrolling");
-}
+(() => {
+  function debounce(callback, dalay) {
+    let timer = null;
+    return function () {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(callback, dalay);
+    };
+  }
 
-function removeScrollClass() {
-  document.body.classList.remove("scrolling");
-}
+  const SCROLLING_OFFSET_THRESHOLD = 500;
 
-function debounce(callback) {
-  var timer = null;
-  return function () {
-    if (timer) {
-      clearTimeout(timer);
+  let scrollStartOffset = null;
+  let isScrolling = false;
+  let scrollStateResetTimer = null;
+
+  function onScrollStart() {
+    document.body.classList.add("scrolling");
+    isScrolling = true;
+  }
+  const resetScrollStartOffset = debounce(function () {
+    scrollStartOffset = null;
+  }, 200);
+  const onScrollStop = debounce(function stopScroll() {
+    if (isScrolling) {
+      document.body.classList.remove("scrolling");
+      document.body.classList.add("stopping");
+      isScrolling = false;
+      scrollStateResetTimer = setTimeout(() => {
+        onScrollReset();
+      }, 300);
     }
-    timer = setTimeout(callback, 40);
-  };
-}
+  }, 1000);
+  function onScrollReset() {
+    document.body.classList.remove("stopping");
+    scrollStateResetTimer = null;
+  }
+  let lastTime = 0;
+  function onScroll() {
+    lastTime = Date.now();
+    if (scrollStartOffset === null) {
+      scrollStartOffset = window.scrollY;
+    }
+    if (
+      Math.abs(window.scrollY - scrollStartOffset) >
+        SCROLLING_OFFSET_THRESHOLD &&
+      isScrolling === false
+    ) {
+      onScrollStart();
+    }
+    resetScrollStartOffset();
+    onScrollStop();
+  }
 
-var debounceRemoveScrollClass = debounce(removeScrollClass);
-
-function onBodyScroll() {
-  addScrollingClass();
-  debounceRemoveScrollClass();
-}
-
-window.addEventListener("scroll", onBodyScroll);
+  window.addEventListener("scroll", onScroll);
+})();
